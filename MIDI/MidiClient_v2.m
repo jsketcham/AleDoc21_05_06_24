4d25600baaa47f6166b576c374bc1d1d0666e41a
Jim Ketcham <jsketcham@earthlink.net>
2024-09-29 09:19:21 -0700

    MidiClien.midiTx now handles messages > 256 bytes

--- MIDI/MidiClient_v2.m
+++ MIDI/MidiClient_v2.m
@@ -486,67 +486,118 @@ static void notifyProc(const MIDINotification *message,
 //}
 //NSTimer *midiTxTimer;
 
+//-(void)midiTx:(NSData*)data{
+//    
+//    if(outPort == PORT_NOT_OPEN || dest == 0) return;    // not open for tx, exit
+//    
+//    // debugging dropped MIDI to UFX
+////    if([title isEqualToString:@"UFX"]){
+////        
+////        if(!midiTxTimer || !midiTxTimer.isValid){
+////            midiTxByteCtr = 0;
+////            midiTxTimer = [NSTimer scheduledTimerWithTimeInterval:0.3 target: self selector:@selector(midiTxTimerService) userInfo:nil repeats: false];
+////        }
+////        midiTxByteCtr += data.length;   // debugging missing UFX rx
+////    }
+//    
+//    Byte *buffer = (Byte*)[data bytes];
+//    int len = (int)[data length];
+//    
+//    
+////    [self performSelectorOnMainThread:@selector(traceMidiData:) withObject:data waitUntilDone:false];
+//    
+//    MIDIPacket* packet = MIDIPacketListInit (&packet_list);
+//    
+//    packet = MIDIPacketListAdd (&packet_list,
+//                                sizeof(packet_list),
+//                                packet,
+//                                0,  // timestamp
+//                                len,
+//                                buffer);
+//    
+//    MIDIFlushOutput(dest);
+//    
+//    MIDISend (
+//              outPort,
+//              dest,
+//              &packet_list
+//              );
+//    
+//}
+//static void SourceReadProc (const MIDIPacketList   *pktlist,
+//                            void                   *readProcRefCon,
+//                            void                   *srcConnRefCon)
+//{
+//    
+//    MidiClient *midiClient = (__bridge MidiClient*)readProcRefCon;
+//    
+//    MIDIPacket *packet = (MIDIPacket*)&pktlist->packet[0];
+//    
+//    for (int i = 0; i < pktlist->numPackets; ++i) {
+//        
+//        int len = packet->length;
+//        Byte *buffer = (Byte*)packet->data;
+//        
+//        @autoreleasepool {  // we believe that this at the top of the call tree should cover all objects created in the tree
+//            
+//            [midiClient decodeData:[NSData dataWithBytes:buffer length:len]];
+//        }
+//        
+//        packet = MIDIPacketNext(packet);    // per 'quick help' for MIDIPacketList
+//        
+//    }
+//}
 -(void)midiTx:(NSData*)data{
     
     if(outPort == PORT_NOT_OPEN || dest == 0) return;    // not open for tx, exit
     
-    // debugging dropped MIDI to UFX
-//    if([title isEqualToString:@"UFX"]){
-//        
-//        if(!midiTxTimer || !midiTxTimer.isValid){
-//            midiTxByteCtr = 0;
-//            midiTxTimer = [NSTimer scheduledTimerWithTimeInterval:0.3 target: self selector:@selector(midiTxTimerService) userInfo:nil repeats: false];
-//        }
-//        midiTxByteCtr += data.length;   // debugging missing UFX rx
-//    }
-    
+    // 09/08/24 send <256 byte packets, then remainder
+
+    MIDIPacketList packet_list;
+    MIDIPacket* packet = MIDIPacketListInit (&packet_list);
+
     Byte *buffer = (Byte*)[data bytes];
     int len = (int)[data length];
     
-    
-//    [self performSelectorOnMainThread:@selector(traceMidiData:) withObject:data waitUntilDone:false];
-    
-    MIDIPacket* packet = MIDIPacketListInit (&packet_list);
-    
-    packet = MIDIPacketListAdd (&packet_list,
-                                sizeof(packet_list),
-                                packet,
-                                0,  // timestamp
-                                len,
-                                buffer);
-    
     MIDIFlushOutput(dest);
-    
-    MIDISend (
-              outPort,
-              dest,
-              &packet_list
-              );
-    
-}
-static void SourceReadProc (const MIDIPacketList   *pktlist,
-                            void                   *readProcRefCon,
-                            void                   *srcConnRefCon)
-{
-    
-    MidiClient *midiClient = (__bridge MidiClient*)readProcRefCon;
-    
-    MIDIPacket *packet = (MIDIPacket*)&pktlist->packet[0];
-    
-    for (int i = 0; i < pktlist->numPackets; ++i) {
+
+    for(int i = 0; i < len; i++){
         
-        int len = packet->length;
-        Byte *buffer = (Byte*)packet->data;
-        
-        @autoreleasepool {  // we believe that this at the top of the call tree should cover all objects created in the tree
+        // packet.data is a 256 byte tuple
+        // break packets on message boundaries (like MIDIEventPacket)
+        // sysex, here, sends remainder so we have an empty buffer for sysex
+      if(((buffer[i] & 0x80) && i > 250) || ((buffer[i] == SYSTEM_EXCLUSIVE) && packet->length > 0)){
             
-            [midiClient decodeData:[NSData dataWithBytes:buffer length:len]];
+            MIDISend (
+                      outPort,
+                      dest,
+                      &packet_list
+                      );
+            
+            packet = MIDIPacketListInit (&packet_list);
+       }
+        
+        
+        packet = MIDIPacketListAdd (&packet_list,
+                                    sizeof(packet_list),
+                                    packet,
+                                    0,  // timestamp
+                                    1,
+                                    &buffer[i]);
+        
+        if(i == (len - 1) || buffer[i] == MIDI_EOX){    // SYSEX must be <= 256 bytes
+                        
+            MIDISend (
+                      outPort,
+                      dest,
+                      &packet_list
+                      );
+            
+            packet = MIDIPacketListInit (&packet_list);
         }
-        
-        packet = MIDIPacketNext(packet);    // per 'quick help' for MIDIPacketList
-        
     }
 }
+
 -(void)decodeData:(NSData*)data{
     
 //    if([title isEqualToString:@"Status"]){
